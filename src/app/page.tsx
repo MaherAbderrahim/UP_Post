cd0d9a75da51e770a0d25a0533a1a9ed3eff19ff
Haroun Malek Hammami <harounmelekh@gmail.com>
2024-04-25 10:39:52 +0100

    clerk authentication and front check point

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ const solutions = [
     description: "Connect with third-party tools that you're already using.",
     href: '#',
     icon: QuestionMarkCircleIcon,
-  },
+  },        
 ]
 const features = [
   {
@@ -286,11 +286,11 @@ export default function Example() {
               </a>
             </Popover.Group>
             <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-              <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+              <a href="\sign-in" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
                 Sign in
               </a>
               <a
-                href="#"
+                href="\sign-up"
                 className="ml-8 whitespace-nowrap inline-flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white hover:from-purple-700 hover:to-indigo-700"
               >
                 Sign up
