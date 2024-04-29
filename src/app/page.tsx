bf02994d92f8a410ae38dc70a784a6fe96cfc217
Haroun Malek Hammami <harounmelekh@gmail.com>
2024-04-29 01:13:40 +0100

    landing about done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,21 +21,88 @@ import {
   AnnotationIcon,
   ChatAlt2Icon,
   ChatAltIcon,
+  CheckIcon,
   DocumentReportIcon,
+  GlobeAltIcon,
   HeartIcon,
   InboxIcon,
+  LightningBoltIcon,
   MenuIcon,
   PencilAltIcon,
   QuestionMarkCircleIcon,
   ReplyIcon,
+  ScaleIcon,
+  SparklesIcon,
   TrashIcon,
   UsersIcon,
   XIcon,
 } from '@heroicons/react/outline'
-import { ChevronDownIcon } from '@heroicons/react/solid'
+import { ChevronDownIcon  } from '@heroicons/react/solid'
 import Link from 'next/link'
 import { ContainerScroll } from '@/components/ui/container-scroll-animation'
 import Image from "next/image";
+
+
+const tiers = [
+  {
+    name: 'Standard',
+    href: '#',
+    priceMonthly: 49,
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+    features: [
+      'Pariatur quod similique',
+      'Sapiente libero doloribus modi nostrum',
+      'Vel ipsa esse repudiandae excepturi',
+      'Itaque cupiditate adipisci quibusdam',
+    ],
+  },
+  {
+    name: 'Enterprise',
+    href: '#',
+    priceMonthly: 79,
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+    features: [
+      'Pariatur quod similique',
+      'Sapiente libero doloribus modi nostrum',
+      'Vel ipsa esse repudiandae excepturi',
+      'Itaque cupiditate adipisci quibusdam',
+    ],
+  },
+]
+
+const transferFeatures = [
+  {
+    id: 1,
+    name: 'Competitive exchange rates',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: GlobeAltIcon,
+  },
+  {
+    id: 2,
+    name: 'No hidden fees',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: ScaleIcon,
+  },
+  {
+    id: 3,
+    name: 'Transfers are instant',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: LightningBoltIcon,
+  },
+]
+
+const communicationFeatures = [
+  {
+    id: 1,
+    name: 'Mobile notifications',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: LightningBoltIcon,
+  }
+]
 
 const solutions = [
   {
@@ -390,12 +457,12 @@ export default function Example() {
               </div>
               <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
                 <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
-                  <span className="block text-white">Take control of your</span>
-                  <span className="block text-indigo-200">customer support</span>
+                  <span className="block text-white">Optimisez vos publications </span>
+                  <span className="block text-indigo-200">pour un engagement maximal</span>
                 </h1>
                 <p className="mt-6 max-w-lg mx-auto text-center text-xl text-indigo-200 sm:max-w-3xl">
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt
-                  amet fugiat veniam occaecat fugiat aliqua.
+                Obtenez une version optimisée de vos publications grâce à notre service. 
+                Assurez-vous un meilleur engagement et une plus grande portée avec notre plateforme.
                 </p>
                 <div className="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
                   <div className="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
@@ -403,7 +470,7 @@ export default function Example() {
                       href="/sign-up"
                       className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8"
                     >
-                      Get started
+                      Commencer
                     </a>
                   </div>
                 </div>
@@ -456,26 +523,26 @@ export default function Example() {
         titleComponent={
           <>
             <h1 className="text-4xl font-semibold text-black dark:text-white">
-              Unleash the power of <br />
+            Suivez l'évolution de vos publications <br />
               <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
-                Scroll Animations
+              en temps réel
               </span>
             </h1>
           </>
         }
       >
         <Image
-          src={`/dashboard.png`}
+          src={`/dashboard_1.png`}
           alt="hero"
           height={720}
-          width={1400}
+          width={1200}
           className="mx-auto rounded-2xl object-cover h-full object-left-top"
           draggable={false}
         />
       </ContainerScroll>
     </div>
         <div className="relative pt-16 pb-32 overflow-hidden">
-          <div aria-hidden="true" className="absolute inset-x-0 top-0 h-48 bg-gradient-to-b from-gray-100" />
+          <div aria-hidden="true" className="absolute inset-x-0 top-0 h-48 " />
           <div className="relative">
             <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
               <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
@@ -487,19 +554,19 @@ export default function Example() {
                   </div>
                   <div className="mt-6">
                     <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
-                      Stay on top of customer support
+                    Évaluation Dynamique de Performance
                     </h2>
                     <p className="mt-4 text-lg text-gray-500">
-                      Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis bibendum malesuada faucibus lacinia
-                      porttitor. Pulvinar laoreet sagittis viverra duis. In venenatis sem arcu pretium pharetra at.
-                      Lectus viverra dui tellus ornare pharetra.
+                        Notre plateforme évalue la performance de chaque publication, fournissant des insights précieux sur leur efficacité. 
+                        Cela permet aux utilisateurs de comprendre ce qui fonctionne et ce qui ne fonctionne pas, 
+                        les aidant à affiner leur stratégie de contenu.
                     </p>
                     <div className="mt-6">
                       <a
-                        href="#"
+                        href="/sign-up"
                         className="inline-flex bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white hover:from-purple-700 hover:to-indigo-700"
                       >
-                        Get started
+                        Commencer
                       </a>
                     </div>
                   </div>
@@ -508,8 +575,7 @@ export default function Example() {
                   <blockquote>
                     <div>
                       <p className="text-base text-gray-500">
-                        &ldquo;Cras velit quis eros eget rhoncus lacus ultrices sed diam. Sit orci risus aenean
-                        curabitur donec aliquet. Mi venenatis in euismod ut.&rdquo;
+                        &ldquo;je suis vraiment impressionné par cette fonctionnalité. L’évaluation de la performance de chaque publication est un outil inestimable pour toute stratégie de contenu.&rdquo;
                       </p>
                     </div>
                     <footer className="mt-3">
@@ -533,13 +599,211 @@ export default function Example() {
                 <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
                   <img
                     className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                    src="https://tailwindui.com/img/component-images/inbox-app-screenshot-1.jpg"
+                    src="/posts.png"
                     alt="Inbox user interface"
                   />
                 </div>
               </div>
             </div>
           </div>
+          <div className="mt-24">
+        <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+          <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
+            <div>
+              <div>
+                <span className="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
+                  <SparklesIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                </span>
+              </div>
+              <div className="mt-6">
+                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
+                Améliorez vos publications avec notre plateforme innovante
+                </h2>
+                <p className="mt-4 text-lg text-gray-500">
+                Pour les publications qui n'atteignent pas leur plein potentiel, notre service utilise des algorithmes avancés pour les améliorer. 
+                En passant par notre Service, les utilisateurs peuvent obtenir une version optimisée de leurs publications, 
+                garantissant un meilleur engagement et une plus grande portée.
+                </p>
+                <div className="mt-6">
+                  <a
+                    href="#"
+                    className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
+                  >
+                    Commencer
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
+            <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+              <img
+                className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
+                src="/upgrade_post.png"
+                alt="Customer profile user interface"
+              />
+            </div>
+          </div>
+        </div>
+        </div>
+        <div className="relative pt-16 pb-32 overflow-hidden">
+          <div aria-hidden="true" className="absolute inset-x-0 top-0 h-48 " />
+          <div className="relative">
+            <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+              <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
+                <div>
+                  <div>
+                    <span className="h-12 w-12 rounded-md flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-600">
+                      <LightningBoltIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                    </span>
+                  </div>
+                  <div className="mt-6">
+                    <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
+                    Créez du contenu attrayant à partir de zéro
+                    </h2>
+                    <p className="mt-4 text-lg text-gray-500">
+                    Notre plateforme offre la possibilité de créer du contenu à partir de zéro. 
+                    Les utilisateurs peuvent fournir un thème ou une idée, et nous nous chargeons de générer une publication 
+                    pertinente et attrayante pour leur public cible.
+
+                    </p>
+                    <div className="mt-6">
+                      <a
+                        href="/sign-up"
+                        className="inline-flex bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white hover:from-purple-700 hover:to-indigo-700"
+                      >
+                        Commencer
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-8 border-t border-gray-200 pt-6">
+                  <blockquote>
+                    <div>
+                      <p className="text-base text-gray-500">
+                        &ldquo;Je trouve cette fonctionnalité extrêmement précieuse. La possibilité de créer du contenu 
+                        à partir de zéro est un véritable game-changer.&rdquo;
+                      </p>
+                    </div>
+                    <footer className="mt-3">
+                      <div className="flex items-center space-x-3">
+                        <div className="flex-shrink-0">
+                          <img
+                            className="h-6 w-6 rounded-full"
+                            src="https://images.unsplash.com/photo-1509783236416-c9ad59bae472?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80"
+                            alt=""
+                          />
+                        </div>
+                        <div className="text-base font-medium text-gray-700">
+                          Elinore Hamlton, Digital Marketing Manager
+                        </div>
+                      </div>
+                    </footer>
+                  </blockquote>
+                </div>
+              </div>
+              <div className="mt-12 sm:mt-16 lg:mt-0">
+                <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+                  <img
+                    className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                    src="/create_result.png"
+                    alt="Inbox user interface"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+          {/* pricing section */}
+          <div className="bg-gray-900">
+      <div className="pt-12 sm:pt-16 lg:pt-24">
+        <div className="max-w-7xl mx-auto text-center px-4 sm:px-6 lg:px-8">
+          <div className="max-w-3xl mx-auto space-y-2 lg:max-w-none">
+            <h2 className="text-lg leading-6 font-semibold text-gray-300 uppercase tracking-wider">Pricing</h2>
+            <p className="text-3xl font-extrabold text-white sm:text-4xl lg:text-5xl">
+              The right price for you, whoever you are
+            </p>
+            <p className="text-xl text-gray-300">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum sequi unde repudiandae natus.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="mt-8 pb-12 bg-gray-50 sm:mt-12 sm:pb-16 lg:mt-16 lg:pb-24">
+        <div className="relative">
+          <div className="absolute inset-0 h-3/4 bg-gray-900" />
+          <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="max-w-md mx-auto space-y-4 lg:max-w-5xl lg:grid lg:grid-cols-2 lg:gap-5 lg:space-y-0">
+              {tiers.map((tier) => (
+                <div key={tier.name} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+                  <div className="px-6 py-8 bg-white sm:p-10 sm:pb-6">
+                    <div>
+                      <h3
+                        className="inline-flex px-4 py-1 rounded-full text-sm font-semibold tracking-wide uppercase bg-indigo-100 text-indigo-600"
+                        id="tier-standard"
+                      >
+                        {tier.name}
+                      </h3>
+                    </div>
+                    <div className="mt-4 flex items-baseline text-6xl font-extrabold">
+                      ${tier.priceMonthly}
+                      <span className="ml-1 text-2xl font-medium text-gray-500">/mo</span>
+                    </div>
+                    <p className="mt-5 text-lg text-gray-500">{tier.description}</p>
+                  </div>
+                  <div className="flex-1 flex flex-col justify-between px-6 pt-6 pb-8 bg-gray-50 space-y-6 sm:p-10 sm:pt-6">
+                    <ul role="list" className="space-y-4">
+                      {tier.features.map((feature) => (
+                        <li key={feature} className="flex items-start">
+                          <div className="flex-shrink-0">
+                            <CheckIcon className="h-6 w-6 text-green-500" aria-hidden="true" />
+                          </div>
+                          <p className="ml-3 text-base text-gray-700">{feature}</p>
+                        </li>
+                      ))}
+                    </ul>
+                    <div className="rounded-md shadow">
+                      <a
+                        href={tier.href}
+                        className="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-gray-900"
+                        aria-describedby="tier-standard"
+                      >
+                        Get started
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="mt-4 relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 lg:mt-5">
+          <div className="max-w-md mx-auto lg:max-w-5xl">
+            <div className="rounded-lg bg-gray-100 px-6 py-8 sm:p-10 lg:flex lg:items-center">
+              <div className="flex-1">
+                <div>
+                  <h3 className="inline-flex px-4 py-1 rounded-full text-sm font-semibold tracking-wide uppercase bg-white text-gray-800">
+                    Discounted
+                  </h3>
+                </div>
+                <div className="mt-4 text-lg text-gray-600">
+                  Get full access to all of standard license features for solo projects that make less than $20k gross
+                  revenue for <span className="font-semibold text-gray-900">$29</span>.
+                </div>
+              </div>
+              <div className="mt-6 rounded-md shadow lg:mt-0 lg:ml-10 lg:flex-shrink-0">
+                <a
+                  href="#"
+                  className="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-gray-900 bg-white hover:bg-gray-50"
+                >
+                  Buy Discounted License
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
         </div>
       </main>
 
