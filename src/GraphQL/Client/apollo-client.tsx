ddbaf34de16856bdec0c22aec6d32c7aa247da37
Haroun Malek Hammami <harounmelekh@gmail.com>
2024-05-09 12:17:49 +0100

    Update Apollo packages and register Apollo client in Providers component

--- src/GraphQL/Client/apollo-client.tsx
+++ src/GraphQL/Client/apollo-client.tsx
@@ -1,15 +1,19 @@
 "use client";
 import React from "react";
-// npm i @apollo/client
-// npm i @apollo/space-kit // for styling
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   const graphqlUrl = "http://localhost:3000/api/graphql";
-  const apolloClient = new ApolloClient({
-    uri: graphqlUrl,
-    cache: new InMemoryCache(),
+
+  const { getClient } = registerApolloClient(() => {
+    return new ApolloClient({
+      uri: graphqlUrl,
+      cache: new InMemoryCache(),
+    });
   });
+
+  const apolloClient = getClient();
 
   return <ApolloProvider client={apolloClient}>{children}</ApolloProvider>;
 };
